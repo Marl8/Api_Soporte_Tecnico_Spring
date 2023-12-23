49d7494e8132c2c4bc1de25a8a3ed297b1f2385f
Marl <winering@gmail.com>
2023-12-23 16:18:37 -0300

    Commit 1

--- src/main/java/com/utn/repository/MesaAyudaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utn.repository;
-
-import com.utn.entity.MesaAyuda;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MesaAyudaRepository extends JpaRepository<MesaAyuda, Long> {
-}
